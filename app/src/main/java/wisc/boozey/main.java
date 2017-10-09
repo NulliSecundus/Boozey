dda3352ec9e2580446687792dab80bce906058e6
NulliSecundus <brendandrackly@gmail.com>
2017-10-09 02:11:28 -0500

    Initial commit, working UI with nav menu working

---
+++ app/src/main/java/wisc/boozey/main.java
@@ -0,0 +1,116 @@
+package wisc.boozey;
+
+import android.app.FragmentManager;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class main extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    private NavigationView navigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.nav_view);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.main_layout, new GameFragment())
+                .commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_game_screen, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        FragmentManager fragmentManager = getFragmentManager();
+
+        //noinspection SimplifiableIfStatement
+
+        if (id == R.id.workshop) {
+            fragmentManager.beginTransaction()
+                    .replace(R.id.main_layout, new WorkshopFragment())
+                    .commit();
+            navigationView.setCheckedItem(R.id.nav_workshop);
+            return true;
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        FragmentManager fragmentManager = getFragmentManager();
+
+        if (id == R.id.nav_games) {
+            fragmentManager.beginTransaction()
+                    .replace(R.id.main_layout, new GameFragment())
+                    .commit();
+        } else if (id == R.id.nav_profile) {
+            fragmentManager.beginTransaction()
+                    .replace(R.id.main_layout, new ProfileFragment())
+                    .commit();
+        } else if (id == R.id.nav_social) {
+            fragmentManager.beginTransaction()
+                    .replace(R.id.main_layout, new SocialFragment())
+                    .commit();
+        } else if (id == R.id.nav_workshop) {
+            fragmentManager.beginTransaction()
+                    .replace(R.id.main_layout, new WorkshopFragment())
+                    .commit();
+        } else if (id == R.id.nav_settings) {
+            fragmentManager.beginTransaction()
+                    .replace(R.id.main_layout, new SettingsFragment())
+                    .commit();
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
