59aea9847da01f4d3028214178812d17a191d202
cscheaffer <scheafferchris@yahoo.com>
2017-11-15 14:19:40 -0600

    Facebook Integration

--- app/src/main/java/wisc/boozey/SettingsFragment.java
+++ app/src/main/java/wisc/boozey/SettingsFragment.java
@@ -1,28 +1,62 @@
 package wisc.boozey;
-
+import android.content.Intent;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import com.androidquery.AQuery;
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import android.view.View.OnClickListener;
 import android.widget.TextView;
-
+import android.widget.Button;
 import org.w3c.dom.Text;
 
 /**
  *
  */
-
-public class SettingsFragment extends Fragment {
+public class SettingsFragment extends Fragment implements OnClickListener{
     View myView;
+    private CallbackManager callbackManager;
+    private TextView text;
+    private LoginButton loginButton;
 
-    @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         myView = inflater.inflate(R.layout.settings_screen, container, false);
         return myView;
+    }
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        Button newBlockButton = (Button) getActivity().findViewById(R.id.Logout);
+        newBlockButton.setOnClickListener((OnClickListener) this);
+    }
+    @Override
+    public void onClick(View v) {
+        LoginManager.getInstance().logOut();
+        Intent intent = new Intent(getActivity(), LoginActivity.class);
+        startActivity(intent);
     }
 }
